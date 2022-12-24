41ce45abbde8912c6e52c3b172a46087bbd08392
Christian <bouwensec@gmail.com>
2022-12-24 12:25:04 -0500

    next + tailwind + stripe boilerplate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,153 @@
+import { MouseEventHandler, useState } from "react";
+
+import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { postRequest } from "utils/postRequest";
+import { getStripe } from "utils/getStripe";
 
-export default function Home() {
+const Home: NextPage = () => {
+  const mItem = {
+    name: "Nike Airforce 1",
+    image:
+      "https://images.unsplash.com/photo-1600269452121-4f2416e55c28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bmlrZSUyMHNob2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    price: 200,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, quia!",
+    quantity: 1,
+  };
+
+  const [item, setItem] = useState(mItem);
+  const [loading, setLoading] = useState(false);
+
+  const decreaseQuantity: MouseEventHandler<HTMLButtonElement> = () => {
+    setItem((item) => ({
+      ...item,
+      quantity: item.quantity > 1 ? item.quantity - 1 : item.quantity,
+    }));
+  };
+
+  const increaseQuantity: MouseEventHandler<HTMLButtonElement> = () => {
+    setItem((item) => ({ ...item, quantity: item.quantity + 1 }));
+  };
+
+  const checkout = async () => {
+    setLoading(true);
+
+    // Create a Checkout Session.
+    const response = await postRequest("/api/checkout-session", { item });
+
+    if (response.statusCode === 500) {
+      console.error(response.message);
+      return;
+    }
+
+    // Redirect to Checkout.
+    const stripe = await getStripe();
+    const { error } = await stripe!.redirectToCheckout({
+      // Make the id field from the Checkout Session creation API response
+      // available to this file, so you can provide it as parameter here
+      // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+      sessionId: response.id,
+    });
+    // If `redirectToCheckout` fails due to a browser or network
+    // error, display the localized error message to your customer
+    // using `error.message`.
+    console.warn(error.message);
+    setLoading(false);
+  };
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>The Lathered Dragon</title>
+        <meta name="description" content="Soap for the modern reptile." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
-        <h1 className={styles.title}>The Lathered Dragon</h1>
+        <h1 className="text-3xl font-semibold text-center mt-10 text-gray-400">
+          The Lathered Dragon
+        </h1>
+        <div className="mt-8 bg-white shadow-xl rounded-lg ring-1 ring-gray-100 max-w-sm mx-auto relative">
+          <Image
+            src={item.image}
+            alt={item.name}
+            height="80"
+            width="100"
+            // layout="responsive"
+            // objectFit="cover"
+            className="rounded-tl-lg rounded-tr-lg"
+          />
 
-        <p className={styles.description}>Soap for the modern reptile.</p>
+          <div className="px-4 py-3">
+            <h5 className="text-xl font-semibold">{item.name}</h5>
+            <p className="text-sm text-gray-400">{item.description}</p>
+            <div className="flex items-center justify-between mt-3">
+              <h6 className="text-3xl font-bold">
+                ${item.price * item.quantity}
+              </h6>
+              <div className="flex items-center space-x-3">
+                <button
+                  className="decrease__quantity p-1 rounded-full ring-1 ring-gray-200"
+                  onClick={decreaseQuantity}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4 text-gray-500"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Coffee</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+                <span className="quantity">{item.quantity}</span>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Lavender</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+                <button
+                  className="increase__quantity p-1 rounded-full ring-1 ring-gray-200"
+                  onClick={increaseQuantity}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4 text-gray-500"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Spruce</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a href="https://vercel.com/new" className={styles.card}>
-            <h3>Lemon Poppyseed</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            {loading ? (
+              <button
+                type="button"
+                className="mt-6 rounded-md py-2 px-3 w-full text-white shadow-lg bg-blue-500 shadow-blue-200 uppercase text-sm hover:ring-1 hover:ring-blue-500"
+              >
+                Processing...
+              </button>
+            ) : (
+              <button
+                type="button"
+                className="mt-6 rounded-md py-2 px-3 w-full text-white shadow-lg bg-blue-500 shadow-blue-200 uppercase text-sm hover:ring-1 hover:ring-blue-500"
+                onClick={checkout}
+              >
+                Checkout
+              </button>
+            )}
+          </div>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
-}
+};
+
+export default Home;
