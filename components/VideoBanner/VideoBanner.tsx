b01b97898449a09c56fc798c1c2bf7499edc4fd3
Christian Bouwense <bouwensec@gmail.com>
2022-12-28 15:47:19 -0500

    720

--- components/VideoBanner/VideoBanner.tsx
+++ components/VideoBanner/VideoBanner.tsx
@@ -4,7 +4,7 @@ export type VideoBannerProps = {
 
 const VideoBanner = () => {
   return (
-    <div className="h-[80vh] w-full max-w-[1440px] flex justify-center z-10">
+    <div className="h-[80vh] w-full max-w-[720px] flex justify-center z-10">
       <video autoPlay muted loop className="object-cover w-full h-full">
         <source src={`making-soap.webm`} type="video/webm" />
       </video>
