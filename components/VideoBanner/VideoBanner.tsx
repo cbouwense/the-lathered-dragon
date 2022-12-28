6abc5577931ca23c4e58310a867abd0d719369b3
Christian Bouwense <bouwensec@gmail.com>
2022-12-28 13:38:43 -0500

    rearrange stripes

--- components/VideoBanner/VideoBanner.tsx
+++ components/VideoBanner/VideoBanner.tsx
@@ -1,43 +1,12 @@
-import { useEffect, useRef, useState } from "react";
+export type VideoBannerProps = {
+  videoNumber: number;
+}
 
-export type Testimonial = {
-  text: string;
-  person: string;
-};
-
-const testimonials: Testimonial[] = [
-  {
-    text: "FABULOUS! I have itchy dry skin when it gets cold and this actually helps.",
-    person: "Jen",
-  },
-  {
-    text: "We absolutely love it. I will definitely be ordering it again.",
-    person: "Trent",
-  }
-];
-
-const VideoBanner = () => {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const [testimonialIndex, setTestimonialIndex] = useState(0);
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTestimonialIndex((p) => (p + 1) % testimonials.length)
-      videoRef.current?.load();
-      videoRef.current?.play();
-    }, 5000);
-
-    return () => { clearInterval(timer) }
-  }, []);
-
-  useEffect(() => {
-    console.log(testimonialIndex)
-  }, [testimonialIndex])
-
+const VideoBanner = ({ videoNumber }: VideoBannerProps) => {
   return (
     <div className="h-[80vh] w-full flex justify-center z-10">
-      <video ref={videoRef} autoPlay muted loop className="object-cover w-full h-full">
-        <source src={`making-soap-raw${testimonialIndex+2}.mp4`} type="video/mp4" />
+      <video autoPlay muted loop className="object-cover w-full h-full">
+        <source src={`making-soap-raw${videoNumber}.mp4`} type="video/mp4" />
       </video>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,26 +17,20 @@ const Home: NextPage = () => {
       </Head>
       <main className="p-0 bg-gradient-to-t from-tan to-dark-tan">
         <Navbar />
-        <VideoBanner />
-        <div className={`${styles.testimonials} relative w-screen h-[50vh] border-box`}>
-          <div className="bg-dark-slate w-full h-full absolute z-10 -skew-y-6 flex justify-center">
-            <h1 className="text-3xl lg:text-5xl font-semibold text-white mt-6">The talk of the town</h1>
-          </div>
+        <VideoBanner videoNumber={3} />
+        <div className={`${styles.testimonials} relative w-full h-[65vh]`}>
+          <div className="bg-dark-slate w-full h-full absolute z-10 -skew-y-6 flex justify-center" />
           <div className="container mx-auto h-full flex flex-col items-center justify-center gap-6 p-6">
+            <h1 className="text-3xl lg:text-5xl font-semibold text-white z-20">The talk of the town</h1>
             <p className="text-xl lg:text-3xl font-semibold text-white z-20">"FABULOUS! I have itchy dry skin when it gets cold and this actually helps." - Jen</p>
             <p className="text-xl lg:text-3xl font-semibold text-white z-20">"We absolutely love it. I will definitely be ordering it again." - Trent</p>
           </div>
         </div>
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 py-16 box-border mt-[30vh]">
-          {items.map((item) => (
-            <Card key={item.id} item={item} />
-          ))}
-        </div>
-        <div className={`${styles.ingredients} relative w-screen h-[50vh] border-box`}>
-          <div className="bg-dark-slate w-full h-full absolute z-10 -skew-y-6 flex justify-center">
-            <h1 className="text-3xl lg:text-5xl font-semibold text-white mt-6">Delightfully real ingredients</h1>
-          </div>
+        <br />
+        <div className={`${styles.ingredients} relative w-full h-[65vh]`}>
+          <div className="bg-dark-slate w-full h-full absolute z-10 -skew-y-6 flex justify-center" />
           <div className="container mx-auto mt-6 w-full h-full flex flex-col items-center justify-center gap-1 p-6">
+            <h1 className="text-3xl lg:text-5xl font-semibold text-white z-20 mb-10">Delightfully real ingredients</h1>
             <p className="text-xl lg:text-3xl font-semibold text-white z-20">Shea butter</p>
             <p className="text-xl lg:text-3xl font-semibold text-white z-20">Cocoa butter</p>
             <p className="text-xl lg:text-3xl font-semibold text-white z-20">Olive oil</p>
@@ -44,6 +38,11 @@ const Home: NextPage = () => {
             <p className="text-xl lg:text-3xl font-semibold text-white z-20">Avocado oil</p>
             <p className="text-xl lg:text-3xl font-semibold text-white z-20">Castor oil</p>
           </div>
+        </div>
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 py-16 box-border mt-[30vh]">
+          {items.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
         </div>
       </main>
     </div>
