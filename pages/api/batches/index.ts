a5fe32bf64665b9f7d2abbf9b45add5973dd35b0
Christian Bouwense <bouwensec@gmail.com>
2023-02-06 21:57:15 -0500

    added param for pourDate

--- pages/api/batches/index.ts
+++ pages/api/batches/index.ts
@@ -1,4 +1,5 @@
-import { batches } from "data";
+import { Batch, batches } from "data";
+import { DateTime } from "luxon";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -11,30 +12,42 @@ export default async function handler(
     return;
   }
 
+  if (Object.keys(req.query).length === 0) {
+    res.status(200).json(batches);
+    return;
+  }
+
+  const batchesMatchingQuery = new Set<Batch>();
+
   if (Object.keys(req.query).includes("id")) {
     const id = Number(req.query.id);
     const batch = batches.find((batch) => batch.id === id);
     if (batch) {
-      res.status(200).json(batch);
-      return;
+      batchesMatchingQuery.add(batch);
     }
-    res.status(404).json({ statusCode: 404, message: "Batch not found" });
-    return;
   }
 
   if (Object.keys(req.query).includes("name")) {
     const name = String(req.query.name);
-    const batch = batches.filter((batch) => batch.name?.includes(name));
-    if (batch) {
-      res.status(200).json(batch);
-      return;
-    }
-    res.status(404).json({ statusCode: 404, message: "Batch not found" });
-    return;
+    batches.forEach((batch) => {
+      console.log({ name: batch.name, matches: batch.name?.includes(name) })
+      if (batch.name?.includes(name)) {
+        batchesMatchingQuery.add(batch);
+      }
+    });
+  }
+
+  if (Object.keys(req.query).includes("pourDate")) {
+    const pourDate = DateTime.fromISO(String(req.query.pourDate));
+    batches.forEach((batch) => {
+      if (batch.pourDate === pourDate) {
+        batchesMatchingQuery.add(batch);
+      }
+    });
   }
 
   try {
-    res.status(200).json(batches);
+    res.status(200).json([...batchesMatchingQuery]);
   } catch (err) {
     const errorMessage =
       err instanceof Error ? err.message : "Internal server error";
